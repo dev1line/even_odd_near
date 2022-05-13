7748144f9bf2d2533f8f87026b4b074e41b9bf6b
Nguyễn Tuấn Quang Sang <sang.nguyen@napaglobal.com>
2022-05-13 17:52:20 +0700

    interface

--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,36 @@
+use more_asserts::{assert_le, assert_gt};
+use near_contract_standards::fungible_token::core::FungibleTokenCore;
 use near_contract_standards::fungible_token::resolver::FungibleTokenResolver;
+use near_contract_standards::non_fungible_token::enumeration::NonFungibleTokenEnumeration;
+// use near_contract_standards::non_fungible_token::enumeration::NonFungibleTokenEnumeration;
+use near_sdk::env::{current_account_id, predecessor_account_id};
 use std::convert::TryInto;
 // use near_contract_standards::non_fungible_token::metadata::TokenMetadata;
 // use near_contract_standards::non_fungible_token::{Token, TokenId};
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::collections::{LazyOption, UnorderedMap};
+use near_sdk::collections::{UnorderedMap};
 
 use near_sdk::json_types::{ValidAccountId, U128};
 use near_sdk::serde::{Deserialize, Serialize};
-use near_sdk::{env, init, near_bindgen, setup_alloc, AccountId, Balance, PanicOnDefault};
+use near_sdk::{env, near_bindgen, setup_alloc, AccountId, Balance, PanicOnDefault};
+use rand::Rng;
+
 pub mod ticket;
 pub mod token;
 use ticket::Ticket;
 use token::Token;
+
+
 setup_alloc!();
+
 const TOTAL_SUPPLY: Balance = 1_000_000_000_000_000;
-// pub trait TicketInterface {
-//     /// Returns true if token should be returned to `sender_id`
-//     pub fn nft_mint(
-//         &mut self,
-//         token_id: TokenId,
-//         receiver_id: ValidAccountId,
-//         token_metadata: TokenMetadata,
-//     ) -> Token;
-// }
+
+
+
 #[derive(Serialize, Deserialize, BorshDeserialize, BorshSerialize, PanicOnDefault)]
 #[serde(crate = "near_sdk::serde")]
 pub struct PlayerMetadata {
-    bet_amount: usize,
+    bet_amount: U128,
     player: AccountId,
     is_even: bool,
 }
@@ -34,7 +38,7 @@ pub struct PlayerMetadata {
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
 pub struct EvenOddContract {
-    owner: AccountId,
+    owner: ValidAccountId,
     cash: Token,
     ticket: Ticket,
     players_array: Vec<AccountId>,
@@ -47,10 +51,11 @@ pub struct EvenOddContract {
 #[near_bindgen]
 impl EvenOddContract {
     #[init]
-    pub fn constructor(dealer: AccountId) -> Self {
+    pub fn constructor() -> Self {
         use near_sdk::env::signer_account_id;
+        assert!(!env::state_exists(), "The contract is already initialized");
         Self {
-            owner: dealer,
+            owner: signer_account_id().try_into().unwrap(),
             cash: Token::new_default_meta(
                 signer_account_id().try_into().unwrap(),
                 TOTAL_SUPPLY.into(),
@@ -63,6 +68,7 @@ impl EvenOddContract {
             roll_id: 1,
         }
     }
+
     #[payable]
     pub fn transfer(&mut self, amount: U128) {
         // assert!()
@@ -73,16 +79,117 @@ impl EvenOddContract {
             amount,
         );
     }
-    pub fn withdraw() {}
-    pub fn bet() {}
-    pub fn roll_dice() {}
-    pub fn is_already_bet() {}
-    pub fn get_dealer_balance() {}
-    pub fn get_bet_amount_of() {}
-    pub fn get_player_info() {}
-    pub fn transfer_token() {}
-    pub fn reset_board() {}
-    pub fn generate_random_number() {}
+
+    pub fn withdraw(&mut self, amount: U128) {
+        use near_sdk::env::{current_account_id, predecessor_account_id};
+        assert_gt!(u128::from(amount), 0, "Amount must be not zero!");
+        assert_le!(u128::from(amount), u128::from(self.get_dealer_balance()), "Amount exceeds balance");
+        self.cash.ft_resolve_transfer(
+            current_account_id().try_into().unwrap(), 
+            predecessor_account_id().try_into().unwrap(), 
+            amount
+        );
+
+        env::log(
+            format!(" Withdraw from {} to {} with amount is {} ", 
+                String::from(current_account_id()), 
+                String::from(predecessor_account_id()),
+                u128::from(amount)
+            ).as_bytes()
+        );
+    }
+
+    pub fn bet(&self, is_even: bool, amount: U128) {
+        // let account_balance = env::account_balance();
+        // let token_id = 
+        let balance = u128::from(Ticket::default().nft_supply_for_owner(predecessor_account_id().try_into().unwrap()));
+        assert_gt!(balance, 0, "You need to buy a ticket to play this game");
+        // assert_le!(env::block_timestamp(), self.ticket.get_expired_time(token_id), "Your ticket is expired");
+        assert!(!self.is_already_bet(predecessor_account_id()) == true, "Already bet");
+
+    }
+    pub fn roll_dice(&mut self) {
+        assert_eq!(self.owner, env::predecessor_account_id().try_into().unwrap(), "Only owner can call roll dice");
+        assert_gt!(self.total_bet_amount_per_roll, 0, "No one place bet");
+
+        let dice_number_1 = self.generate_random_number();
+        let dice_number_2 = self.generate_random_number();
+
+        let is_even = (dice_number_1 + dice_number_2) %2 == 0;
+
+        env::log(
+            format!(" Roll id: {}, Roll dice: {} - {} with resul is {}", 
+              self.roll_id, 
+              dice_number_1,
+              dice_number_2,
+                is_even
+            ).as_bytes()
+        );
+        for i in 0..self.players_array.len() {
+            if self.players.get(&self.players_array[i]).unwrap().is_even == is_even {
+                self.transfer_money(
+                    self.players_array[i].clone(), 
+                    U128::from(
+                        u128::from(
+                            self.players.get(
+                                &self.players_array[i]
+                            ).unwrap().bet_amount
+                        )*2
+                    )
+                );
+            }
+        }
+        self.reset_board();
+        self.roll_id = self.roll_id + 1;
+    }
+    pub fn is_already_bet(&self, account: AccountId) -> bool {
+        if u128::from(self.players.get(&account).unwrap().bet_amount) > 0 {
+            return true;
+        }
+        false
+    }
+    pub fn get_dealer_balance(&self) -> U128 {
+        self.cash.ft_balance_of(self.owner.clone())
+    }
+
+    pub fn get_bet_amount_of(&self, account: AccountId) -> U128 {
+        self.players.get(&account).unwrap().bet_amount
+    }
+
+    pub fn get_player_info(&self, account: AccountId) -> (U128, bool) {
+        let bet_amount = self.players.get(&account).unwrap().bet_amount;
+        let is_even = self.players.get(&account).unwrap().is_even;
+        (bet_amount, is_even)
+    }
+
+    #[private]
+    pub fn transfer_money(&mut self, account: AccountId, amount: U128) {
+       
+        if account.clone().eq(&String::from(self.owner.clone())) {
+            self.cash.ft_resolve_transfer(
+                current_account_id().try_into().unwrap(), 
+                predecessor_account_id().try_into().unwrap(), 
+                amount
+            );
+        } else {
+            self.cash.ft_resolve_transfer(
+                current_account_id().try_into().unwrap(), 
+                self.players.get(&account).unwrap().player.try_into().unwrap(), 
+                amount
+            );
+        }
+    }
+
+    #[private]
+    pub fn reset_board(&mut self) {
+        self.players_array = vec![];
+        self.total_bet_amount_per_roll = 0;
+    }
+
+    #[private]
+    pub fn generate_random_number(&self) -> usize {
+        rand::thread_rng().gen_range(0..100)
+    }
 }
 
 // #[cfg(all(test, not(target_arch = "wasm32")))]
--- src/ticket.rs
+++ src/ticket.rs
@@ -21,7 +21,7 @@ use near_contract_standards::non_fungible_token::metadata::{
 use near_contract_standards::non_fungible_token::{Token, TokenId};
 use near_contract_standards::non_fungible_token::NonFungibleToken;
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::collections::LazyOption;
+use near_sdk::collections::{LazyOption, UnorderedMap};
 use near_sdk::json_types::ValidAccountId;
 use near_sdk::{
     env, near_bindgen, AccountId, BorshStorageKey, PanicOnDefault, Promise, PromiseOrValue,
@@ -30,15 +30,16 @@ use near_sdk::{
 near_sdk::setup_alloc!();
 
 #[near_bindgen]
-#[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
+#[derive( BorshDeserialize, BorshSerialize, PanicOnDefault)]
 
 pub struct Ticket {
     tokens: NonFungibleToken,
     metadata: LazyOption<NFTContractMetadata>,
+    expired_time: UnorderedMap<TokenId, u64>,
 }
 
 const DATA_IMAGE_SVG_NEAR_ICON: &str = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 288 288'%3E%3Cg id='l' data-name='l'%3E%3Cpath d='M187.58,79.81l-30.1,44.69a3.2,3.2,0,0,0,4.75,4.2L191.86,103a1.2,1.2,0,0,1,2,.91v80.46a1.2,1.2,0,0,1-2.12.77L102.18,77.93A15.35,15.35,0,0,0,90.47,72.5H87.34A15.34,15.34,0,0,0,72,87.84V201.16A15.34,15.34,0,0,0,87.34,216.5h0a15.35,15.35,0,0,0,13.08-7.31l30.1-44.69a3.2,3.2,0,0,0-4.75-4.2L96.14,186a1.2,1.2,0,0,1-2-.91V104.61a1.2,1.2,0,0,1,2.12-.77l89.55,107.23a15.35,15.35,0,0,0,11.71,5.43h3.13A15.34,15.34,0,0,0,216,201.16V87.84A15.34,15.34,0,0,0,200.66,72.5h0A15.35,15.35,0,0,0,187.58,79.81Z'/%3E%3C/g%3E%3C/svg%3E";
-
+const DURATION: u64 = 60*60*24*30;
 #[derive(BorshSerialize, BorshStorageKey)]
 enum StorageKey {
     NonFungibleToken,
@@ -81,6 +82,7 @@ impl Ticket {
                 Some(StorageKey::Approval),
             ),
             metadata: LazyOption::new(StorageKey::Metadata, Some(&metadata)),
+            expired_time: UnorderedMap::new(b"expire_time".to_vec()),
         }
     }
 
@@ -99,7 +101,12 @@ impl Ticket {
         receiver_id: ValidAccountId,
         token_metadata: TokenMetadata,
     ) -> Token {
+        self.expired_time.insert(&token_id, &(env::block_timestamp() + DURATION));
         self.tokens.mint(token_id, receiver_id, Some(token_metadata))
+    }
+
+    pub fn get_expired_time(&self, token_id: TokenId) -> u64 {
+        self.expired_time.get(&token_id).unwrap()
     }
 }
 
